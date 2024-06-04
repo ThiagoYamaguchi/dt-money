1fe0186553a356563c1032b1a337a72920d72d81
“Thiago <thiago.osamu@outlook.com>
2024-06-04 02:02:46 -0300

    first commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,39 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
+export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75e68" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Entradas</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>R$ 17.400,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
